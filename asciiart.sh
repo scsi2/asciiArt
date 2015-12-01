5b7ede4affb469741175e2969f871696bb28fac0
antoine <antoineccmm@msn.com>
2015-12-01 18:32:47 -0500

    ajout du ascii art du prof

---
+++ asciiart.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+function etu1 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu2 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu3 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu4 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu5 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu6 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu7 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu8 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu9 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu10 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu11 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu12 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu13 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu14 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu15 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu16 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu17 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu18 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu19 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu20 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu21 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu22 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu23 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu24 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu25 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu26 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu27 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu28 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu29 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu30 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function etu31 {
+	echo "travail de"
+	echo "mon asci art"
+	sleep $1
+	clear
+}
+function prof {
+	echo "travail du prof"
+	echo "mon asci art"
+	echo		    ..
+	echo	      ..  ..
+	echo	            ..
+	echo	             ..
+	echo	            ..
+	echo	           ..
+	echo	         ..
+	echo "	##       ..    ####"
+	echo "	##.............##  ##"
+	echo "	##.............##   ##"
+	echo "	##.............## ##"
+	echo "	##.............###"
+	echo "	 ##...........##"
+	echo "	  #############"
+	echo "	  #############"
+	echo "	#################"
+	sleep $1
+	clear
+}
+
+#le chiffre passé en paramètre est le temps que votre ascii art sera affiché
+prof 1
